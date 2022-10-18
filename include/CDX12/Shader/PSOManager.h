f1ea6204e449d154d15ae084e9172297f3bf4bf3
WuYichen <2950335254@qq.com>
2022-10-19 00:07:47 +0800

    add: createPSO

--- include/CDX12/Shader/PSOManager.h
+++ include/CDX12/Shader/PSOManager.h
@@ -14,6 +14,14 @@ namespace Chen::CDX12 {
             DXGI_FORMAT mBackBufferFormat,
 		    DXGI_FORMAT mDepthStencilFormat);
 
+        void CreatePipelineState(
+            std::string name,
+            ID3D12Device* device,
+            BasicShader* shader,
+            UINT rtNum,
+            DXGI_FORMAT mBackBufferFormat,
+            DXGI_FORMAT mDepthStencilFormat);
+
         ID3D12PipelineState* GetPipelineState(const std::string& name) const {
             if (mPSOs.find(name) != mPSOs.end()) {
                 return mPSOs.at(name).Get();
@@ -24,7 +32,6 @@ namespace Chen::CDX12 {
         }
     private:
         std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
-        ID3D12Device* md3dDevice;
         D3D12_GRAPHICS_PIPELINE_STATE_DESC basePsoDesc;
     };
 }
--- src/core/Shader/PSOManager.cpp
+++ src/core/Shader/PSOManager.cpp
@@ -19,4 +19,54 @@ PSOManager::PSOManager(
             mBackBufferFormat,
             mDepthStencilFormat);
     ThrowIfFailed(device->CreateGraphicsPipelineState(&basePsoDesc, IID_PPV_ARGS(&mPSOs["base"])));
+
+    //
+    // PSO for Transparent
+    //
+
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC transparentPsoDesc = basePsoDesc;
+    transparentPsoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+    transparentPsoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+
+    D3D12_RENDER_TARGET_BLEND_DESC transparencyBlendDesc;
+    transparencyBlendDesc.BlendEnable = true;
+    transparencyBlendDesc.LogicOpEnable = false;
+
+    transparencyBlendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+    transparencyBlendDesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+    transparencyBlendDesc.BlendOp = D3D12_BLEND_OP_ADD;
+
+    transparencyBlendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+    transparencyBlendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+    transparencyBlendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+
+    transparencyBlendDesc.LogicOp = D3D12_LOGIC_OP_NOOP;
+    transparencyBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+    transparentPsoDesc.BlendState.RenderTarget[0] = transparencyBlendDesc;
+    ThrowIfFailed(device->CreateGraphicsPipelineState(&transparentPsoDesc, IID_PPV_ARGS(&mPSOs["transparent"])));
+}
+
+void PSOManager::CreatePipelineState(
+    std::string name,
+    ID3D12Device* device,
+    BasicShader* shader,
+    UINT rtNum,
+    DXGI_FORMAT mBackBufferFormat,
+    DXGI_FORMAT mDepthStencilFormat)
+{
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc
+        = Desc::PSO::MRT(
+            shader->RootSig(),
+            shader->mInputLayout.data(),
+            (UINT)shader->mInputLayout.size(),
+            shader->vsShader.Get(),
+            shader->psShader.Get(),
+            rtNum,
+            mBackBufferFormat,
+            mDepthStencilFormat);
+    psoDesc.RasterizerState = shader->rasterizerState;
+    psoDesc.BlendState = shader->blendState;
+    psoDesc.DepthStencilState = shader->depthStencilState;
+    ThrowIfFailed(device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPSOs[name])));
 }
